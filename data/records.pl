f22b8bf27c706057ee14d0f3839b74305d9ac030
John Filippone <johnpfilippone@yahoo.com>
2017-02-04 16:11:02 -0600

    Combined input parser and csv generator

--- data/records.pl
+++ data/records.pl
@@ -1,1 +1,2 @@
-{"date": "04/02/2017", "minutes": "parse_machine.py", "tags": ["40", "groceries"]}
+{"date": "04/02/2017", "minutes": "15", "tags": ["reading"]}
+{"date": "04/02/2017", "minutes": "30", "tags": ["working"]}
--- src/parse_machine.py
+++ src/parse_machine.py
@@ -7,29 +7,33 @@ def add_record(args):
 
     record = {
         "date": time.strftime("%d/%m/%Y"),
-        "minutes": sys.argv[0],
-        "tags": sys.argv[1:]}
+        "minutes": args[0],
+        "tags": args[1:]}
 
     with open("../data/records.pl", "a") as f:
         f.write(json.dumps(record) + "\n")
 
 
+def generate_csv():
+    with open("../data/records.pl", "r") as f:
+        lines = f.readlines()
+
+    with open("/var/www/html/time-machine.csv", "w") as f:
+        f.write("tag,population\n")
+    with open("/var/www/html/time-machine.csv", "a") as f:
+        for line in lines:
+            record = json.loads(line)
+            f.write(str(record["tags"][0]) + "," + str(record["minutes"]) + "\n")
+
 
 
 if __name__ == "__main__":
-    if sys.argv[1] == "-c":
-        with open("../data/records.pl", "r") as f:
-            lines = f.readlines()
 
-        with open("/var/www/html/time-machine.csv", "w") as f:
-            f.write("tag,population\n")
-        with open("/var/www/html/time-machine.csv", "a") as f:
-            for line in lines:
-                record = json.loads(line)
-                f.write(str(record["tags"][0]) + "," + str(record["minutes"]) + "\n")
-    else:
-        add_record(sys.argv[1:])
-     
+    add_record(sys.argv[1:])
+    generate_csv()
+
+    
+ 
         
     
     
