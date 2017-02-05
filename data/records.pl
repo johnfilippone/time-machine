9f5b9cc1fa1eb2db7d5d9f36e9de3740512a897f
John Filippone <johnpfilippone@yahoo.com>
2017-02-04 19:32:35 -0600

    Updates todo

--- data/records.pl
+++ data/records.pl
@@ -4,3 +4,5 @@
 {"date": "04/02/2017", "minutes": "30", "tags": ["singing"]}
 {"date": "04/02/2017", "minutes": "60", "tags": ["singing"]}
 {"date": "04/02/2017", "minutes": "20", "tags": ["prioritizing"]}
+{"date": "04/02/2017", "minutes": "10", "tags": ["thinking"]}
+{"date": "04/02/2017", "minutes": "120", "tags": ["lifing-weights"]}
