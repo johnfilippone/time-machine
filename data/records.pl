a675069fbf7d9f260aa5baa4c44ab5476199577b
John Filippone <johnpfilippone@yahoo.com>
2017-02-04 19:03:46 -0600

    updates todo

--- data/records.pl
+++ data/records.pl
@@ -1,3 +1,5 @@
 {"date": "04/02/2017", "minutes": "65", "tags": ["time-machine"]}
 {"date": "04/02/2017", "minutes": "45", "tags": ["school"]}
 {"date": "04/02/2017", "minutes": "45", "tags": ["school"]}
+{"date": "04/02/2017", "minutes": "30", "tags": ["singing"]}
+{"date": "04/02/2017", "minutes": "60", "tags": ["singing"]}
